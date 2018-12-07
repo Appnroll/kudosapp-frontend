b0804f8c59f6b94964d90641fff517124a0d70e8
Kamil Gajowy <kamil.gajowy@gmail.com>
2018-12-07 13:29:26 +0100

    make the life more colorful

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
 import Colors from "../constants/Colors";
-import { Route, Link, withRouter } from "react-router-dom";
+import { Route, NavLink, withRouter } from "react-router-dom";
 import Logo from './Logo'
 
 export const headerHeight = 70
@@ -13,10 +13,11 @@ class Header extends Component {
                 <Logo/>
                 <Menu>
                     <li>
-                        <Link to="/wall">Wall</Link>
+                        <NavLink to="/wall" activeStyle={{ color: 'red' }}>
+                            Wall</NavLink>
                     </li>
                     <li>
-                        <Link to="/stats">Statistics</Link>
+                        <NavLink to="/stats" activeStyle={{ color: 'red' }}>Statistics</NavLink>
                     </li>
                 </Menu>
             </StyledHeader>
