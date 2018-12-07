e84a145787252e4e276e742ee12be89d0fb7f382
ulaulala <ukazmierczyk@gmail.com>
2018-12-07 15:23:44 +0100

    Updates

---
+++ precache-manifest.6fcbef4e479b8c4936a6fa8451846dd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97f2d5d1b834f1e37cfb",
+    "url": "/kup-kudosapp-frontend/static/js/runtime~main.97f2d5d1.js"
+  },
+  {
+    "revision": "18024ebc4d2147750dfe",
+    "url": "/kup-kudosapp-frontend/static/js/main.18024ebc.chunk.js"
+  },
+  {
+    "revision": "48d1ebd6494b64354c8b",
+    "url": "/kup-kudosapp-frontend/static/js/1.48d1ebd6.chunk.js"
+  },
+  {
+    "revision": "18024ebc4d2147750dfe",
+    "url": "/kup-kudosapp-frontend/static/css/main.a1a8c108.chunk.css"
+  },
+  {
+    "revision": "c85dc612c03a7a01ef586cf1d673f2c4",
+    "url": "/kup-kudosapp-frontend/index.html"
+  }
+];
