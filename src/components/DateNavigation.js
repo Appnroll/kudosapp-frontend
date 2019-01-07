69e4129998b0c37848383b6bfdeed11abfc29326
Mateusz Kocz <mateusz@kocz.pl>
2019-01-07 15:06:01 +0100

    Style the list

--- src/components/DateNavigation.js
+++ src/components/DateNavigation.js
@@ -1,41 +1,56 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import { NavLink } from "react-router-dom";
 import Colors from "../constants/Colors";
 import { getMonthNameByIndex } from "../utils/months";
-import {getYears} from "../utils/years";
+import { getYears } from "../utils/years";
 
 class DateNavigation extends Component {
     render () {
         const { currentYear, currentMonth } = this.props
         return (
-            <nav>
-                <ul>
+            <Navigation>
+                <List>
                     {
                         getYears().map(year =>
-                            <li key={year}>
+                            <Item key={year}>
                                 <NavLink to={`${process.env.PUBLIC_URL}/stats/givers/${year}/${currentMonth}`}
                                          activeStyle={{color: Colors.Banana}} key={year}>
                                     {year}
                                 </NavLink>
-                            </li>
+                            </Item>
                         )
                     }
-                </ul>
-                <ul>
+                </List>
+                <List supplementary>
                     {
                         Array(12).fill(0).map((month, index) =>
-                            <li key={index}>
+                            <Item key={index}>
                                 <NavLink to={`${process.env.PUBLIC_URL}/stats/givers/${currentYear}/${index + 1}`}
                                          activeStyle={{color: Colors.Banana}}>
                                     {getMonthNameByIndex(index)}
                                 </NavLink>
-                            </li>
+                            </Item>
                         )
                     }
-                </ul>
-            </nav>
+                </List>
+            </Navigation>
         )
     }
 }
+
+const Navigation = styled.nav`
+  margin: 2rem 0;
+`
+
+const List = styled.ul`
+  display: flex;
+  justify-content: center;
+  font-size: ${props => props.supplementary ? 1.2 : 1.5}rem;
+`
+
+const Item = styled.li`
+  padding: .5em;
+`
 
 export default DateNavigation
