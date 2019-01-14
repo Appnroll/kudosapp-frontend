3b379329b4f7f7605436a3896d86c5659ead8648
ulaulala <ukazmierczyk@gmail.com>
2019-01-14 11:09:03 +0100

    fix-draft: NavLink paths changed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter as Router } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
         <App />
     </Router>, document.getElementById('root'));
 
