a2ce8fee24ee95daf586d960e74d7985cc3d8cd9
Mateusz Kocz <mateusz@kocz.pl>
2019-02-01 16:16:04 +0100

    Fetch single months

--- src/components/KudosStatsRequest.js
+++ src/components/KudosStatsRequest.js
@@ -1,31 +1,23 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import Request, { RequestMaker } from './Request'
-import { getMonthIndexByAbbreviation } from '../utils/months'
 
 export default class KudosStatsRequest extends Component {
-    static propTypes = RequestMaker.handlersPropTypes
-    static defaultProps = RequestMaker.defaultProps
+    static propTypes = {
+        ...RequestMaker.handlersPropTypes,
+        year: PropTypes.string.isRequired,
+        month: PropTypes.string.isRequired
+    }
 
-    normalize = stats =>
-        stats.reduce((acc, {year, month, from, quantity}) => {
-            // TODO: when API changes returned data, this will have to change.
-            const monthIndex = getMonthIndexByAbbreviation(month)
-            if (!acc[year]) {
-                acc[year] = {}
-            }
-            if (!acc[year][monthIndex]) {
-                acc[year][monthIndex] = []
-            }
-            acc[year][monthIndex].push({from, quantity: Number(quantity)})
-            return acc
-        }, {})
+    static defaultProps = RequestMaker.defaultProps
 
     render() {
         return (
             <Request
                 authorized
-                from='kudos/from'
-                then={response => this.props.then(this.normalize(response))}
+                params={{year: this.props.year, month: this.props.month}}
+                from='kudos/from/:year/:month'
+                then={this.props.then}
                 catch={this.props.catch}
             />
         )
