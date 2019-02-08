f218c8a1b8fcf361bf44765c378ca14f4959b253
Mateusz Kocz <mateusz@kocz.pl>
2019-02-08 12:14:36 +0100

    Remove providers from the main file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
 import Wrapper from "./components/Wrapper";
-import Authorization from './components/Authorization'
-import Networking from './components/Networking'
 
 class App extends Component {
     render() {
         return (
-            <Authorization>
-                <Networking>
-                    <Wrapper/>
-                </Networking>
-            </Authorization>
+            <Wrapper/>
         );
     }
 }
