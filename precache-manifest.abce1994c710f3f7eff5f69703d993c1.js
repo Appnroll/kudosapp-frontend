f7df2d8c2afbf7fb20f78f1a93d6f9ea951b80a0
kgajowy <kamil.gajowy@9yards.at>
2019-02-11 14:19:23 +0100

    Updates

--- precache-manifest.abce1994c710f3f7eff5f69703d993c1.js
+++ precache-manifest.abce1994c710f3f7eff5f69703d993c1.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/kudosapp-frontend/static/js/runtime~main.685dca98.js"
   },
   {
-    "revision": "831d7281ca055c81af41",
-    "url": "/kudosapp-frontend/static/js/main.831d7281.chunk.js"
+    "revision": "213124d7c9412fba0908",
+    "url": "/kudosapp-frontend/static/js/main.213124d7.chunk.js"
   },
   {
     "revision": "6213a4451174dcb1fcec",
     "url": "/kudosapp-frontend/static/js/1.6213a445.chunk.js"
   },
   {
-    "revision": "831d7281ca055c81af41",
-    "url": "/kudosapp-frontend/static/css/main.f17289c1.chunk.css"
+    "revision": "213124d7c9412fba0908",
+    "url": "/kudosapp-frontend/static/css/main.b2523f42.chunk.css"
   },
   {
-    "revision": "58245d26609e1d6274af9ee14a29077f",
+    "revision": "4fad9d217551325e44fdbf41e876cf8a",
     "url": "/kudosapp-frontend/index.html"
   }
 ];
