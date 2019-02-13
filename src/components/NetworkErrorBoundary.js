2337a4d3087ed56a8a82790d68d3563c5aae52c5
Mateusz Kocz <mateusz@kocz.pl>
2019-02-13 11:04:41 +0100

    Make the NetworkErrorBoundry a single component responsible for network errords

--- src/components/NetworkErrorBoundary.js
+++ src/components/NetworkErrorBoundary.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+import { withRouter } from 'react-router-dom'
 import ErrorContainer from './ErrorContainer'
+import Colors from '../constants/Colors'
 
-export default class NetworkErrorBoundary extends Component {
+const ErrorMessage = styled.p`
+  margin-bottom: 1em;
+`
+
+const ErrorButton = styled.button`
+  border: 1px solid ${Colors.Banana}; 
+  background: white;
+  color: black;
+  font-size: 1rem;
+  padding: 1rem;
+`
+
+class NetworkErrorBoundary extends Component {
     state = {
         error: false
     }
@@ -12,19 +27,40 @@ export default class NetworkErrorBoundary extends Component {
 
     render() {
         const {error} = this.state
-        if (error) {
-            if (error.status) {
-                return (
-                    <ErrorContainer>
-                        <h1>Network Error <span role="img" aria-label="thunder">⚡️</span>️</h1>
-                        <p>[{error.status}] {error.message}</p>
-                    </ErrorContainer>
-                )
-            } else {
-                throw error
-            }
-        } else {
-            return this.props.children
+
+        if (error && !error.status) {
+            throw error
         }
+
+        return (
+            <>
+                {
+                    error &&
+                    <ErrorContainer>
+                        {
+                            this.isAuthError() ?
+                                <h1>Authentication Error <span role="img" aria-label="spy">🕵️‍♂️</span></h1> :
+                                <h1>Network Error <span role="img" aria-label="thunder">⚡️</span>️</h1>
+                        }
+                        <ErrorMessage>[{error.status}] {error.message}</ErrorMessage>
+                        <ErrorButton onClick={() => this.setState({error: false})}>
+                            Retry
+                        </ErrorButton>
+                        {
+                            this.isAuthError() &&
+                            <ErrorButton onClick={() => this.setState({error: false})}>
+                                Log out
+                            </ErrorButton>
+                        }
+                    </ErrorContainer>
+
+                }
+                {!error && this.props.children}
+            </>
+        )
+    }
+
+    isAuthError() {
+        return this.state.error && this.state.error.status === 403
     }
 }
